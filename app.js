aabde0dd085283d927571d639455c60c775e118e
anshusaurav <anshu.saurav@gmail.com>
2020-07-17 15:04:55 +0530

    Tab Reducer

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 var store = Redux.createStore(rootReducer);
-var input = document.querySelector('input');
+var input = document.querySelector('input[type=text]');
 var list = document.querySelector('ul');
+var all = document.querySelector('.all');
+var active = document.querySelector('.active');
+var completed = document.querySelector('.completed');
 
 function render() {
     var todos = store.getState().todos;
     console.log(todos);
-    var html = todos.map(function(todo) {
-      return '<li id="' + todo.id + '" class="' + (todo.completed ? 'completed' : '') + '">' +
-				'<div class="view">' +
-					'<input class="toggle" type="checkbox" ' + (todo.completed ? 'checked' : '') + '/>' +
-					'<label>' + todo.text + '</label>' +
-					'<button class="destroy"></button>' +
-				'</div>' +
-              '</li>';
+    var html = todos.todoList.map(function(todo) {
+      return `<li id=" ${todo.id} " class=" ${todo.completed ? 'completed' : ''}" >
+				<div class="view">
+		  			
+					<input class="toggle" type="checkbox" ${todo.completed ? 'checked' : ''} />
+					<label>${todo.text}</label>
+					<button class="destroy"></button>
+				</div>
+              </li>`;
     });
     list.innerHTML = html;
 }
 
 input.addEventListener('change', function(e) {
+	console.log('wew');
 	var value = e.target.value;
 	store.dispatch(actions.addTodo(value));
  	e.target.value = '';
@@ -29,14 +34,30 @@ list.addEventListener('click', function(e) {
 	switch (target.tagName) {
 		case 'BUTTON' :
 			id = target.parentNode.parentNode.id;
+
 			store.dispatch(actions.deleteTodo(id));
 	  	break;
 		case 'INPUT' :
 			id = target.parentNode.parentNode.id;
+			console.log(id);
 			store.dispatch(actions.completeTodo(id));
 			break;
 	}
 });
-
+all.addEventListener('click', function(e){
+	store.dispatch({
+		type: 'All'
+	})
+})
+active.addEventListener('click', function(e){
+	store.dispatch({
+		type: 'Active'
+	})
+})
+completed.addEventListener('click', function(e){
+	store.dispatch({
+		type: 'Completed'
+	})
+})
 render();
 store.subscribe(render);
--- reducers.js
+++ reducers.js
@@ -1,35 +1,14 @@
-var initialState = {
-    todoList: [
-        {
-            text: 'Use Redux',
-            completed: false,
-            id: 0
-        },
-        {
-            text: 'Use React',
-            completed: false,
-            id: 1
-        }
-    ],
-    activeTab: `all`
-}
 
-function todos(state, action) {
-    var state = state || initialState;
+
+function todos(state={todoList:[], tab:'All'}, action) {
+
 
     switch (action.type) {
         case types.ADD_TODO:
-            // state.push({
-            //     id: state.length ? state.reduce(function (maxId, todo) {
-            //         return Math.max(todo.id, maxId);
-            //     }, -1) + 1 : 0,
-            //     completed: false,
-            //     text: action.text
-            // });
-            // return state;
+
             return {
-                ...state, ...state.todoList.concat([{
-                    text: action.text, completed: false, id: state.todoList.length ? state.reduce((maxId, todo) => {
+                ...state, todoList: state.todoList.concat([{
+                    text: action.text, completed: false, id: state.todoList.length ? state.todoList.reduce((maxId, todo) => {
                         return Math.max(todo.id, maxId);
                     }, -1) + 1 : 0,
                 }])
@@ -39,41 +18,46 @@ function todos(state, action) {
         case types.DELETE_TODO:
             return {
                 ...state,
-                ...state.todoList.filter( todo => {
-                    return todo.id !== parseInt(action.id, 10);
+                todoList: state.todoList.filter( todo => {
+                    return todo.id !== Number(action.id)
                 })
             }
-        // case types.EDIT_TODO:
-        //     return state.map(function (todo) {
-        //         todo.id === action.id ? Object.assign({}, todo, {text: action.text}) : todo
-        //     });
+
         case types.COMPLETE_TODO:
             return {
                 ...state,
-                ...state.todoList.map(todo => {
-                    return todo.id === parseInt(action.id, 10) ? Object.assign({}, todo, {completed: !todo.completed}) : todo;
+                todoList: state.todoList.map(todo => {
+                    return todo.id === Number(action.id)? Object.assign({}, todo, {completed: !todo.completed}) : todo;
                 })
             }
-        // case types.COMPLETE_ALL:
-        //     var areAllMarked = state.every(function (todo) {
-        //         todo.completed
-        //     });
-        //     return state.map(function (todo) {
-        //         return Object.assign({}, todo, {completed: !areAllMarked});
-        //     });
-        // case types.CLEAR_COMPLETED :
-        //     return state.filter(function (todo) {
-        //         return todo.completed === false;
-        //     });
+        case types.COMPLETE_ALL:
+            var areAllMarked = state.todoList.every(function (todo) {
+                todo.completed
+            });
+            return {...state, todoList: state.todoList.map(function (todo) {
+                return Object.assign({}, todo, {completed: !areAllMarked});
+            })
+            }
+        case types.CLEAR_COMPLETED :
+            return {...state, todoList: state.todoList.filter(function (todo) {
+                return todo.completed === false;
+            })}
 
         default:
             return state
     }
 }
 
-function changeTabs(state, action) {
-    var state = state || initialState;
+function changeTabs(state={todoList:[], tab:'All'}, action) {
+    switch(action.type) {
+        case 'Active':
+            return {...state, tab:'Active'};
+        case 'Completed':
+            return {...state, tab:'Completed'};
+        default:
+            return {...state, tab: 'All'};
+    }
 
 }
 
-var rootReducer = Redux.combineReducers({todos});
+var rootReducer = Redux.combineReducers({todos, changeTabs});
